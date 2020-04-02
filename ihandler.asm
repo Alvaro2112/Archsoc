7f7b74eccb1c2e0ec227ef14bb932b22f5470a2e
alvaro21 <alvarocauderan8515@gmail.com>
2020-04-02 16:46:33 +0200

    save

--- ihandler.asm
+++ ihandler.asm
@@ -2,7 +2,7 @@
 .equ LEDS1, 0x2004
 .equ LEDS2, 0x2008
 .equ TIMER, 0x2020
-.equ EDGECAPTURE, 0x2034 
+.equ EDGECAPTURE, 0x2034
 
 _start:
 br main ; jump to the main function
@@ -15,16 +15,16 @@ stw t1, 4(sp)
 stw ra, 8(sp)
 
 rdctl t0, ctl4
-;test le 3ieme bit 
+;test le 3ieme bit
 andi t1, t0, 0b100
 
-beq zero, t1 , int_timer 
+beq zero, t1 , int_timer
 call isr_button
 
-;test le premier bit 
+;test le premier bit
 
 int_timer:
-andi t1, t0, 1 
+andi t1, t0, 1
 beq t1,zero, int_fin
 call isr_timer
 
@@ -48,7 +48,7 @@ isr_button:
 addi sp, sp, -12
 stw t0, 0(sp)
 stw t1, 4(sp)
-stw ra, 8(sp) 
+stw ra, 8(sp)
 
 ldw t0, EDGECAPTURE(zero)
 stw r0, EDGECAPTURE(zero)
@@ -73,11 +73,13 @@ isr_timer:
 addi sp, sp, -12
 stw t0, 0(sp)
 stw t1, 4(sp)
-stw ra, 8(sp) 
+stw ra, 8(sp)
 
 ldw t0 , LEDS2(zero)
 addi t0 , t0, 1
 stw t0 , LEDS2(zero)
+
+stw zero, TIMER+12(zero)
 ;
 ldw t0, 0(sp)
 ldw t1, 4(sp)
@@ -87,6 +89,9 @@ ret
 
 
 main:
+stw        zero, LEDS0(zero)            ;initialize counters
+stw        zero, LEDS1(zero)
+stw        zero, LEDS2(zero)
 
 ;initialise le stack pointer dans la ram
 addi sp,zero, 0x1500
@@ -100,7 +105,10 @@ addi t1, zero,1
 wrctl ctl0, t1
 
 ; mettre la period
-addi t0, zero, 999
+addi    t0, zero, 999
+
+
+
 stw t0, TIMER+4(zero)
 
 ;mettre le control
