cc153ba9af441cddec494710237de857d2f6a2c6
alvaro21 <alvarocauderan8515@gmail.com>
2020-04-07 17:04:22 +0200

    fixed vhdl

--- ToSubmit/vhdl/control_registers.vhd
+++ ToSubmit/vhdl/control_registers.vhd
@@ -20,7 +20,9 @@ end control_registers;
 
 architecture synth of control_registers is
 
-signal s_stat, s_estat, s_bstat, s_iena, s_epnd: std_logic_vector(31 downto 0);
+signal s_stat, s_estat, s_bstat: std_logic;
+signal s_iena: std_logic_vector(31 downto 0);
+signal s_epnd: std_logic_vector(31 downto 0);
 
 begin
 
@@ -30,32 +32,34 @@ write_clk: process(clk, reset_n, write_n)
     begin
 
         if(reset_n = '0') then
-            s_stat <= X"00000000";
-            s_estat <= X"00000000";
-            s_bstat <= X"00000000";
+            s_stat <= '0';
+            s_estat <= '0';
+            s_bstat <= '0';
             s_iena <= X"00000000";
         
         elsif(rising_edge(clk) and reset_n = '1') then
             if(backup_n /= '1') then
-                s_estat <= s_stat;
-                s_stat <= X"00000000";
+                s_stat <= '0';                
+s_estat <= s_stat;
             end if;
             if(restore_n /= '1') then
                 s_stat <= s_estat;
             end if;
             if(write_n /= '1') then
-                if(unsigned(address) = 0) then s_stat <= wrdata;
-                elsif(unsigned(address) = 1) then s_estat <= wrdata;
-                elsif(unsigned(address) = 2) then s_bstat(1) <= wrdata(1);
+                if(unsigned(address) = 0) then s_stat <= wrdata(0);
+                elsif(unsigned(address) = 1) then s_estat <= wrdata(0);
+                elsif(unsigned(address) = 2) then s_bstat <= wrdata(1);
                 elsif(unsigned(address) = 3) then s_iena <= wrdata;
                 end if;
             end if;
         end if;
     end process;
 
-rddata <= s_stat when unsigned(address) = 0 else
-      s_estat when unsigned(address) = 1 else
-      X"0000000" & "00" & s_bstat(0) & "0" when unsigned(address) = 2 else
+
+
+rddata <= X"0000000" & "000" & s_stat when unsigned(address) = 0 else
+      X"0000000" & "000" & s_estat when unsigned(address) = 1 else
+      X"00000000" when unsigned(address) = 2 else
       s_iena when unsigned(address) = 3 else
       s_epnd when unsigned(address) = 4;
 
@@ -64,14 +68,8 @@ rddata <= s_stat when unsigned(address) = 0 else
 
 
 
-
-
-
-
-s_epnd <= (s_iena and irq);
-ipending <= '1' when s_stat /= X"00000000" and (s_epnd /= X"00000000")else '0';
-
+ipending <= '1' when (s_stat /= '0' and unsigned(s_epnd) /= 0) else '0';
+s_epnd <= s_iena and irq ;
     
-
 
 end synth;
